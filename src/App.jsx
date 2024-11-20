d4c7fdcb9de71e09d70751c008b0a1c455e273fa
Atik2788 <ashanto653.2@gmail.com>
2024-11-20 17:53:15 +0600

    after doing fade up and down animation

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,51 @@ const gridSquireVariants = {
 const App = () => {
   return (
     <div className="fex fex-col gap-10 overflow-auto">
+      {/***************** 01. Div showing slowly using >>> motion.div/section >  gridContainerVariants,  gridSquireVariants********************/}
       <motion.section
         variants={gridContainerVariants}
         initial="hidden"
         animate="show"
         className="grid grid-cols-3 gap-10 p-10"
       >
+        {/************  02. Fade Up Start ***************/}
+        <motion.div
+          variants={gridSquireVariants}
+          className="bg-slate-800 aspect-square rounded-lg flex justify-center items-center gap-10"
+        >
+          <motion.div
+            className="w-20 h-20 bg-stone-100 rounded-lg"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
+          />
+          <motion.div
+            className="w-20 h-20 bg-stone-100 rounded-full"
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeOut", delay: 0.4 }}
+          />
+        </motion.div>
+        {/************  02 Fade Up End ***************/}
+
+        <motion.div
+          variants={gridSquireVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center items-center gap-10"
+        >
+          <motion.div 
+            className="w-1/3 h-1/3 shadow-md bg-rose-400"
+            animate={{
+              scale: [1, 2, 2, 1],
+              
+            }}
+            />
+        </motion.div>
+
+        <motion.div
+          variants={gridSquireVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center items-center gap-10"
+        ></motion.div>
+
         <motion.div
           variants={gridSquireVariants}
           className="bg-slate-800 aspect-square rounded-lg justify-center items-center gap-10"
@@ -38,22 +77,6 @@ const App = () => {
           variants={gridSquireVariants}
           className="bg-slate-800 aspect-square rounded-lg justify-center items-center gap-10"
         ></motion.div>
-        
-        <motion.div
-          variants={gridSquireVariants}
-          className="bg-slate-800 aspect-square rounded-lg justify-center items-center gap-10"
-        ></motion.div>
-
-        <motion.div
-          variants={gridSquireVariants}
-          className="bg-slate-800 aspect-square rounded-lg justify-center items-center gap-10"
-        ></motion.div>
-
-        <motion.div
-          variants={gridSquireVariants}
-          className="bg-slate-800 aspect-square rounded-lg justify-center items-center gap-10"
-        ></motion.div>
-
       </motion.section>
     </div>
   );
